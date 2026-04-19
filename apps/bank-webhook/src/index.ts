c43ae7a5a28e3fa1a6eda211a6b8e1609cd796a2
kishanchaubey23 <chaubeykishan027@gmail.com>
2026-04-20 02:21:47 +0530

    Initial project setup

---
+++ apps/bank-webhook/src/index.ts
@@ -0,0 +1,32 @@
+import express from "express"
+import db from "@repo/db/client"
+const app=express();
+app.post("/hdfcWebhook",async(req,res)=>{
+    const paymentInformation={
+        token:req.body.token,
+        userId:req.body.user_identifier,
+        amount:req.body.amount
+    };
+    await db.balance.update({
+        where:{
+            userId:paymentInformation.userId
+        },
+        data:{
+            amount:{
+                increment:paymentInformation.amount
+            }
+        }
+    });
+    await db.onRampTransaction.update({
+        where:{
+            token:paymentInformation.token,
+        },
+        data:{
+            status:"Success"
+        }
+
+    })
+    res.status(200).json({
+        message:"cpatured"
+    })
+})
